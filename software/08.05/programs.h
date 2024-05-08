347774615f90e832eca5e07775107e3f3563624e
DuarteTavares <166354573+DuarteTavares@users.noreply.github.com>
2024-05-08 21:41:36 +0100

    Add files via upload

---
+++ software/08.05/programs.h
@@ -0,0 +1,61 @@
+#ifndef PROGRAMS_H
+#define PROGRAMS_H
+
+void setup();
+void loop();
+void update_state(int input);
+void handle_program(price_data& price_data, power_data& power_data);
+
+enum plug_state {
+  OFF,
+  ON,
+  STANDBY,
+  ERROR
+};
+
+enum program{
+    program0,
+    program1,
+    program2,
+    program3,
+    program4,
+    program5,
+    program6,
+    program7,
+};
+
+enum Modos{manual=1, scheduled};
+
+enum Regimes{simples=1, bi, tri};
+
+class price_data{
+    
+    public:
+
+        Regimes regime;
+        float price_1;
+        float price_2;
+        float price_difference;
+        float best_price;
+        float worst_price;
+        float current_price; //do function to return
+        float combined_price; //do function to return
+        int best_schedule;
+        
+};
+
+class power_data{
+
+    public:
+
+        int minute;
+        int hour;
+        int minute_dur;
+        int hour_dur;
+        float power_W; // do function to return
+        float power_cost;
+        float custo_kWh;
+        Modos modo;
+};
+
+#endif //PROGRAMS_H
